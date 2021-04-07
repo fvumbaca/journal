dc7f2cbac48639d66cfb582c61e8dea30222d90a
Frank Vumbaca <fvumbaca@outlook.com>
2021-04-07 02:57:14 -0400

    Removed old way of building in version

--- cmd/version.go
+++ cmd/version.go
@@ -3,16 +3,16 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/fvumbaca/journal/version"
 	"github.com/spf13/cobra"
 )
+
+var version = "unreleased"
 
 var versionCMD = &cobra.Command{
 	Use:   "version",
 	Short: "Print version info",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Version:", version.Version)
-		fmt.Println("SHA:", version.SHA)
+		fmt.Println("Version:", version)
 	},
 }
 
--- version/version_gen.go
+++
@@ -1,6 +0,0 @@
-// Generated. DO NOT EDIT
-
-package version
-
-const Version = ""
-const SHA = "7b964b74"
